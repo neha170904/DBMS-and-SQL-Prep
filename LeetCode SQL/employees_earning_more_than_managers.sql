7811e7c7abdf20441e785926305d878c6a3bb531
NEHA SARASWAT <rajkumarsaraswathere1982@gmail.com>
2026-05-31 23:50:40 +0530

    Create employees_earning_more_than_managers.sql

---
+++ LeetCode SQL/employees_earning_more_than_managers.sql
@@ -0,0 +1,9 @@
+-- Problem: Employees Earning More Than Their Managers
+-- Platform: LeetCode
+-- Difficulty: Easy
+
+SELECT e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
