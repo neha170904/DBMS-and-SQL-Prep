fff55aa6204d82d0b768844076acf20730b6ee31
NEHA SARASWAT <rajkumarsaraswathere1982@gmail.com>
2026-06-03 07:53:08 +0530

    Create sales_red.sql

---
+++ LeetCode SQL/sales_red.sql
@@ -0,0 +1,14 @@
+-- Problem: Sales Person
+-- Platform: LeetCode
+-- Difficulty: Easy
+
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN
+(
+    SELECT sales_id
+    FROM Orders
+    JOIN Company
+    ON Orders.com_id = Company.com_id
+    WHERE Company.name = 'RED'
+);
