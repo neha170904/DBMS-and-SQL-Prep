6772e5903f6e96a657b76f3f74ae21b06ac3baec
NEHA SARASWAT <rajkumarsaraswathere1982@gmail.com>
2026-06-03 07:39:18 +0530

    Create most_friends.sql

---
+++ LeetCode SQL/most_friends.sql
@@ -0,0 +1,19 @@
+-- Problem: Friend Requests II - Who Has the Most Friends
+-- Platform: LeetCode
+-- Difficulty: Medium
+
+SELECT id,
+       COUNT(*) AS num
+FROM
+(
+    SELECT requester_id AS id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) AS Friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
